3afafc605ba97a00fef3ece903a91b5f9cfac01a
Julius de Bruijn <bruijn@prisma.io>
2019-09-30 14:38:32 +0200

    Schwomp

--- query-engine/prisma/src/server.rs
+++ query-engine/prisma/src/server.rs
@@ -34,14 +34,15 @@ impl HttpServer {
 
         let request_context = RequestContext {
             context: PrismaContext::new(legacy_mode)?,
-            graphql_request_handler: GraphQlRequestHandler,
+            graphql_request_handler: GraphQlRequestHandler
+        };
 
         let mut app = App::with_state(request_context);
         app.at("/").get(Self::playground_handler).post(Self::http_handler);
         app.at("/sdl").get(Self::sdl_handler);
         app.at("/dmmf").get(Self::dmmf_handler);
         app.at("/status").get(Self::status_handler);
-        app.at("/server_info").get(Self::status_handler);
+        app.at("/server_info").get(Self::server_info_handler);
 
         trace!("Initialized in {}ms", now.elapsed().as_millis());
         info!("Started http server on {}:{}", address.0, address.1);
@@ -86,7 +87,7 @@ impl HttpServer {
     /// Serves a raw SDL string created from the query schema.
     async fn sdl_handler(cx: Context<RequestContext>) -> Response<String> {
         let request_context = cx.state();
-        let rendered = GraphQLSchemaRenderer::render(Arc::clone(&request_context.context().query_schema()));
+        let rendered = GraphQLSchemaRenderer::render(Arc::clone(&request_context.context.query_schema()));
 
         Response::builder()
             .status(200)
@@ -101,8 +102,8 @@ impl HttpServer {
         let request_context = cx.state();
 
         let dmmf = dmmf::render_dmmf(
-            request_context.context().datamodel(),
-            Arc::clone(request_context.context().query_schema()),
+            request_context.context.datamodel(),
+            Arc::clone(request_context.context.query_schema()),
         );
 
         Ok(response::json(dmmf))
@@ -115,7 +116,7 @@ impl HttpServer {
         let response = json!({
             "commit": env!("GIT_HASH"),
             "version": env!("CARGO_PKG_VERSION"),
-            "primary_connector": request_context.context().primary_connector(),
+            "primary_connector": request_context.context.primary_connector(),
         });
 
         Ok(response::json(response))
@@ -125,4 +126,3 @@ impl HttpServer {
         Ok(response::json(json!({"status": "ok"})))
     }
 }
-
